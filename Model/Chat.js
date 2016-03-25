881a11e8e4e9df2df97e3c79979d12afbd21fd9a
Lucas Barbosa <Lucas@MacBook-Pro.local>
2016-03-25 01:50:59 -0700

    Remove wrong attribute on Chat model

--- Model/Chat.js
+++ Model/Chat.js
@@ -2,9 +2,14 @@ var Sequelize = require("sequelize")
 var Database = require("./Database")
 
 Chat = Database.define("chat", {
-	firstUserId: Sequelize.INTEGER,
-	secondUserId: Sequelize.INTEGER,
-	messageId: Sequelize.INTEGER
+	firstUserId: {
+		type: Sequelize.INTEGER,
+		allowNull: false
+	},
+	secondUserId: {
+		type: Sequelize.INTEGER,
+		allowNull: false
+	}
 })
 
 
