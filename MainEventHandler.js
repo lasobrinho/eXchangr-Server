66a6f37cb9350dd007a1bea86c18dcb46d2e343c
Lucas Barbosa <coding.lucas@gmail.com>
2016-04-28 23:37:02 -0700

    Refactor

--- MainEventHandler.js
+++ MainEventHandler.js
@@ -1,25 +1,22 @@
 var ResponseCodes = require('./ResponseCodes')
 
-var UserRegistration = require('./EventHandlers/UserRegistration/UserRegistration')
-var UserAuthentication = require('./EventHandlers/UserAuthentication/UserAuthentication')
-var ItemAddition = require('./EventHandlers/ItemAddition/ItemAddition')
-var ItemBrowsing = require('./EventHandlers/ItemBrowsing/ItemBrowsing')
-var ItemRemoval = require('./EventHandlers/ItemRemoval/ItemRemoval')
-var ItemRetrieval = require('./EventHandlers/ItemRetrieval/ItemRetrieval')
-var ItemDistance = require('./EventHandlers/ItemDistance/ItemDistance')
-var ItemUpdate = require('./EventHandlers/ItemUpdate/ItemUpdate')
+var eventHandlers = [
+    require('./EventHandlers/UserRegistration/UserRegistration'),
+    require('./EventHandlers/UserAuthentication/UserAuthentication'),
+    require('./EventHandlers/ItemAddition/ItemAddition'),
+    require('./EventHandlers/ItemBrowsing/ItemBrowsing'),
+    require('./EventHandlers/ItemRemoval/ItemRemoval'),
+    require('./EventHandlers/ItemRetrieval/ItemRetrieval'),
+    require('./EventHandlers/ItemDistance/ItemDistance'),
+    require('./EventHandlers/ItemUpdate/ItemUpdate'),
+]
 
 module.exports = {
     bindEvents: function(server, database, Error) {
         server.on('connection', function(socket) {
-			UserRegistration.bindEvents(socket, database, ResponseCodes, Error)
-            UserAuthentication.bindEvents(socket, database, ResponseCodes, Error)
-            ItemAddition.bindEvents(socket, database, ResponseCodes, Error)
-            ItemBrowsing.bindEvents(socket, database, ResponseCodes, Error)
-            ItemRemoval.bindEvents(socket, database, ResponseCodes, Error)
-            ItemRetrieval.bindEvents(socket, database, ResponseCodes, Error)
-            ItemDistance.bindEvents(socket, database, ResponseCodes, Error)
-            ItemUpdate.bindEvents(socket, database, ResponseCodes, Error)
+            eventHandlers.forEach(function(handler) {
+                handler.bindEvents(socket, database, ResponseCodes, Error)
+            })
         })
     }
 }
