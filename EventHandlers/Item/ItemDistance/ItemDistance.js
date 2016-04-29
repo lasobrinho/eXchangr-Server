01d7acb5987c3fd3c5051385543ff78c5a88c0c8
Lucas Barbosa <coding.lucas@gmail.com>
2016-04-29 14:31:35 -0700

    Refactoring

--- EventHandlers/Item/ItemDistance/ItemDistance.js
+++ EventHandlers/Item/ItemDistance/ItemDistance.js
@@ -1,18 +1,5 @@
 var events = require('./ItemDistanceEvents')
-
-function calculateDistance(coordinate1, coordinate2) {
-    var R = 3959 // miles
-    var fi_1 = coordinate1.latitude * Math.PI/180
-    var fi_2 = coordinate2.latitude *  Math.PI/180
-    var delta_fi_ = (coordinate2.latitude-coordinate1.latitude) * Math.PI/180
-    var delta_epsilon = (coordinate2.longitude-coordinate1.longitude) * Math.PI/180
-
-    var a = Math.sin(delta_fi_/2) * Math.sin(delta_fi_/2) + Math.cos(fi_1) * Math.cos(fi_2) * Math.sin(delta_epsilon/2) * Math.sin(delta_epsilon/2)
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
-    
-    var d = R * c
-    return Math.round(d * 100) / 100
-}
+var DistanceModule = require('../DistanceModule')
 
 module.exports = {
     bindEvents: function(socket, database, responseCodes, Error) {
@@ -43,7 +30,7 @@ module.exports = {
                             console.log(item)
                             socket.emit(events.out, {
                                 responseCode: responseCodes.success,
-                                distance: calculateDistance(user.coordinate, item.user.coordinate)
+                                distance: DistanceModule.distanceInMiles(user.coordinate, item.user.coordinate)
                             })
                         } else {
                             throw Error('Could not find item')
