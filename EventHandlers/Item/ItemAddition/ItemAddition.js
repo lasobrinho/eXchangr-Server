26d08d245fbca110d9e5fd35a832784b440b94aa
Lucas Barbosa <coding.lucas@gmail.com>
2016-05-01 21:22:30 -0700

    Add user check on item addition

--- EventHandlers/Item/ItemAddition/ItemAddition.js
+++ EventHandlers/Item/ItemAddition/ItemAddition.js
@@ -2,22 +2,29 @@ var events = require('./ItemAdditionEvents')
 
 module.exports = {
     bindEvents: function(socket, database, responseCodes, Error) {
+        var User = database.models.user
         var Item = database.models.item
         var Picture = database.models.picture
 
         socket.on(events.in, function(data) {
-            Item.create({
-                name: data.item.name,
-                description: data.item.description,
-                active: data.item.active,
-                userId: data.user.id,
-                pictures: data.item.pictures
-            }, {
-                include: Picture
-            }).then(function(item) {
-                socket.emit(events.out, {
-                    responseCode: responseCodes.success
-                })
+            User.findById(data.user.id).then(function(user) {
+                if (user != null) {
+                    Item.create({
+                        name: data.item.name,
+                        description: data.item.description,
+                        active: data.item.active,
+                        userId: data.user.id,
+                        pictures: data.item.pictures
+                    }, {
+                        include: Picture
+                    }).then(function(item) {
+                        socket.emit(events.out, {
+                            responseCode: responseCodes.success
+                        })
+                    })
+                } else {
+                    throw Error('Authentication Error')
+                }
             }).catch(function(error) {
                 socket.emit(events.out, {
                     responseCode: responseCodes.internalError,
